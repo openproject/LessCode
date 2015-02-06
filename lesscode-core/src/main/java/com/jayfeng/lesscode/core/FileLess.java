6c8fd4bfa2765e5688b2e541aa6233a2469b6a05
Jay <fjtianxia@gmail.com>
2015-02-06 19:33:20 +0800

    add FileLess

---
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/FileLess.java
@@ -0,0 +1,74 @@
+package com.jayfeng.lesscode.core;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileLess {
+
+    /**
+     * read content from file
+     * @param file
+     * @return string
+     * @throws IOException
+     */
+    public static String $read(File file) throws IOException {
+        String text = null;
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+            text = $read(is);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+        return text;
+    }
+
+    /**
+     * read content from inputstream
+     * @param is
+     * @return string
+     * @throws IOException
+     */
+    public static String $read(InputStream is) throws IOException {
+        StringBuffer strbuffer = new StringBuffer();
+        String line;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(is));
+            while ((line = reader.readLine()) != null) {
+                strbuffer.append(line).append("\r\n");
+            }
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return strbuffer.toString();
+    }
+
+    /**
+     * write string to file
+     * @param file
+     * @param str
+     * @throws IOException
+     */
+    public static void $write(File file, String str) throws IOException {
+        DataOutputStream out = null;
+        try {
+            out = new DataOutputStream(new FileOutputStream(file));
+            out.write(str.getBytes());
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
