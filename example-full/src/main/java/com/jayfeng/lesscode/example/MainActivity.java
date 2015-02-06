93e049ad7617f7cc6dd7395ad1cf5870a719ac91
Jay <fjtianxia@gmail.com>
2015-02-06 11:51:39 +0800

    add the lesscode-full module

---
+++ example-full/src/main/java/com/jayfeng/lesscode/example/MainActivity.java
@@ -0,0 +1,47 @@
+package com.jayfeng.lesscode.example;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.jayfeng.lesscode.full.ViewLess;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = ViewLess.$(this, R.id.hello);
+        ViewLess.$width(textView, 400);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
