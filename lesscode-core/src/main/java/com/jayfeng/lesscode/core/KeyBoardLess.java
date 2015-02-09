b55c62c8c465d4a62581516d01dcb6feadcb288b
Jay <fjtianxia@gmail.com>
2015-02-09 10:11:53 +0800

    add KeyBoardLess

---
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/KeyBoardLess.java
@@ -0,0 +1,21 @@
+package com.jayfeng.lesscode.core;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public class KeyBoardLess {
+
+    public static void $show(Context context, View view) {
+        InputMethodManager imm =
+                (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(view, InputMethodManager.RESULT_SHOWN);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    public static void $hide(Context context, View view) {
+        InputMethodManager imm =
+                (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
