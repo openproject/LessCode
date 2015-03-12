e9a83414e8d40b04d9695de93bbfacb2e7e1f0ce
think <673592063@qq.com>
2015-03-12 23:56:32 +0800

    enhances the LogLess by printing the file name, method name and line number when logging

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/LogLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/LogLess.java
@@ -1,36 +1,67 @@
 package com.jayfeng.lesscode.core;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 public class LogLess {
 
     public static void v(String str) {
         if ($.sDebug) {
-            Log.v($.sTAG, str);
+            Log.v(getTag(), buildLogString(str));
         }
     }
 
     public static void $d(String str) {
         if ($.sDebug) {
-            Log.d($.sTAG, str);
+            Log.d(getTag(), buildLogString(str));
         }
     }
 
     public static void $i(String str) {
         if ($.sDebug) {
-            Log.i($.sTAG, str);
+            Log.i(getTag(), buildLogString(str));
         }
     }
 
     public static void $w(String str) {
         if ($.sDebug) {
-            Log.w($.sTAG, str);
+            Log.w(getTag(), buildLogString(str));
         }
     }
 
     public static void $e(String str) {
         if ($.sDebug) {
-            Log.e($.sTAG, str);
+            Log.e(getTag(), buildLogString(str));
         }
+    }
+
+    private static String getTag() {
+        StackTraceElement caller = new Throwable().fillInStackTrace().getStackTrace()[2];
+        if (TextUtils.isEmpty($.sTAG)) {
+            return caller.getFileName();
+        }
+        return $.sTAG;
+    }
+
+    private static String buildLogString(String str) {
+        StackTraceElement caller = new Throwable().fillInStackTrace().getStackTrace()[2];
+        StringBuilder stringBuilder = new StringBuilder();
+        if (TextUtils.isEmpty($.sTAG)) {
+            stringBuilder.append(caller.getMethodName())
+                    .append("():")
+                    .append(caller.getLineNumber())
+                    .append(":")
+                    .append(str);
+        } else {
+            stringBuilder
+                    .append(caller.getFileName())
+                    .append(".")
+                    .append(caller.getMethodName())
+                    .append("():")
+                    .append(caller.getLineNumber())
+                    .append(":")
+                    .append(str);
+        }
+        return stringBuilder.toString();
     }
 }
