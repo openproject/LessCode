4a3f69bd89fa1006bf1dda22bd95d8858cd1690f
Jay <673592063@qq.com>
2015-04-23 22:44:47 +0800

    fix the preference less error (Srting/Integer error sometimes) and upgrade the core version to 0.2.3

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/SharedPreferenceLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/SharedPreferenceLess.java
@@ -10,46 +10,18 @@ public class SharedPreferenceLess {
 
     public static final String SHARED_NAME = "shared_name";
 
-    public static void $put(Context context, String key, Object object) {
-
+    public static void $put(Context context, String key, String value) {
         SharedPreferences sp = context.getSharedPreferences(SHARED_NAME,
                 Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-
-        if (object instanceof String) {
-            editor.putString(key, (String) object);
-        } else if (object instanceof Integer) {
-            editor.putInt(key, (Integer) object);
-        } else if (object instanceof Boolean) {
-            editor.putBoolean(key, (Boolean) object);
-        } else if (object instanceof Float) {
-            editor.putFloat(key, (Float) object);
-        } else if (object instanceof Long) {
-            editor.putLong(key, (Long) object);
-        } else {
-            editor.putString(key, object.toString());
-        }
-
+        editor.putString(key, value);
         SharedPreferencesCompat.apply(editor);
     }
 
-    public static Object $get(Context context, String key, Object defaultObject) {
+    public static Object $get(Context context, String key, String defaultValue) {
         SharedPreferences sp = context.getSharedPreferences(SHARED_NAME,
                 Context.MODE_PRIVATE);
-
-        if (defaultObject instanceof String) {
-            return sp.getString(key, (String) defaultObject);
-        } else if (defaultObject instanceof Integer) {
-            return sp.getInt(key, (Integer) defaultObject);
-        } else if (defaultObject instanceof Boolean) {
-            return sp.getBoolean(key, (Boolean) defaultObject);
-        } else if (defaultObject instanceof Float) {
-            return sp.getFloat(key, (Float) defaultObject);
-        } else if (defaultObject instanceof Long) {
-            return sp.getLong(key, (Long) defaultObject);
-        }
-
-        return null;
+        return sp.getString(key, defaultValue);
     }
 
     public static void $remove(Context context, String key) {
