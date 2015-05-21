48bc62aeb4bd439f779e1487f1d6e2618ea69933
Jay <673592063@qq.com>
2015-05-22 07:47:54 +0800

    add Dialog ViewLess method

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/ViewLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/ViewLess.java
@@ -1,15 +1,20 @@
 package com.jayfeng.lesscode.core;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.view.View;
 
 public class ViewLess {
 
     public static <T extends View> T $(Activity activity, int viewId) {
-        return (T)activity.findViewById(viewId);
+        return (T) activity.findViewById(viewId);
     }
 
-    public static <T extends View> T $(View parent, int viewId) {
-        return (T)parent.findViewById(viewId);
+    public static <T extends View> T $(View view, int viewId) {
+        return (T) view.findViewById(viewId);
+    }
+
+    public static <T extends View> T $(Dialog dialog, int viewId) {
+        return (T) dialog.findViewById(viewId);
     }
 }
