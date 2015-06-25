d82c7abdaeba51d6d313c981d1e2175e40224bbd
fengjian <fengjian@51muai.com>
2015-06-25 10:03:01 +0800

    support for custom log printing out in case

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/LogLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/LogLess.java
@@ -3,34 +3,44 @@ package com.jayfeng.lesscode.core;
 import android.text.TextUtils;
 import android.util.Log;
 
+/**
+ * support for custom log printing out in case
+ * adb shell setprop log.tag.YOUR_LOG_TAG LEVEL
+ * for thinking of perfomence, it need restart your app
+ *
+ * Example (from system):
+ * adb shell setprop log.tag.SQLiteLog V
+ */
 public class LogLess {
 
+    public static final boolean DEBUG_TAG_LOG = Log.isLoggable($.sTAG, Log.VERBOSE);
+
     public static void v(String str) {
-        if ($.sDebug) {
+        if ($.sDebug || DEBUG_TAG_LOG) {
             Log.v(getTag(), buildLogString(str));
         }
     }
 
     public static void $d(String str) {
-        if ($.sDebug) {
+        if ($.sDebug || DEBUG_TAG_LOG) {
             Log.d(getTag(), buildLogString(str));
         }
     }
 
     public static void $i(String str) {
-        if ($.sDebug) {
+        if ($.sDebug || DEBUG_TAG_LOG) {
             Log.i(getTag(), buildLogString(str));
         }
     }
 
     public static void $w(String str) {
-        if ($.sDebug) {
+        if ($.sDebug || DEBUG_TAG_LOG) {
             Log.w(getTag(), buildLogString(str));
         }
     }
 
     public static void $e(String str) {
-        if ($.sDebug) {
+        if ($.sDebug || DEBUG_TAG_LOG) {
             Log.e(getTag(), buildLogString(str));
         }
     }
