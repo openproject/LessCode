b265099b97d0b044c42a2786d24b35286799e650
Jay <673592063@qq.com>
2015-06-25 10:37:48 +0800

    add l,m,tv,h,xh,xxh dpi check methods to DisplayLess

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/DisplayLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/DisplayLess.java
@@ -32,4 +32,28 @@ public class DisplayLess {
                 >=
                 Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
+
+    public static boolean isLdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_ldpi);
+    }
+
+    public static boolean isMdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_mdpi);
+    }
+
+    public static boolean isTVdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_tvdpi);
+    }
+
+    public static boolean isHdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_hdpi);
+    }
+
+    public static boolean isXHdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_xhdpi);
+    }
+
+    public static boolean isXXHdpi() {
+        return $.sAppContext.getResources().getBoolean(R.bool.less_xxhdpi);
+    }
 }
