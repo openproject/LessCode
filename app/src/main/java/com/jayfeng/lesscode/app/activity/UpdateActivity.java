a5db7bac5cfb283c07db33e394b61d042260e95f
Arthas <497721363@qq.com>
2016-02-05 13:49:29 +0800

    a

--- app/src/main/java/com/jayfeng/lesscode/app/activity/UpdateActivity.java
+++ app/src/main/java/com/jayfeng/lesscode/app/activity/UpdateActivity.java
@@ -14,9 +14,9 @@ public class UpdateActivity extends Activity {
         setContentView(R.layout.activity_update);
 
         String updateJson = "{\n" +
-                "      \"vercode\":112,\n" +
+                "      \"vercode\":2,\n" +
                 "      \"vername\":\"V1.1\",\n" +
-                "      \"download\":\"http://www.apk.anzhi.com/data3/apk/201506/09/3a978f27369b4a8bf6de1270da9871ec_86281300.apk\",\n" +
+                "      \"download\":\"http://download.ctrip.com/client/apppackage/Ctrip_1108_4008_9z1m24.apk\",\n" +
                 "      \"log\":\"upgrade content\"\n" +
                 "      }";
         UpdateLess.$check(this, updateJson);
