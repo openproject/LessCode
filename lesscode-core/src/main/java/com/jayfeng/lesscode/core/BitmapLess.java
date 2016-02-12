6a8e0fbfc05243063d96eb93c07f9a8b1d4189b3
fengjian <fengjian@51muai.com>
2016-02-12 19:01:44 +0800

    add $inSampleSize to BitmapLess for calculate the best inSampleSize by reqWidth and reqHeight

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/BitmapLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/BitmapLess.java
@@ -1,4 +1,30 @@
 package com.jayfeng.lesscode.core;
 
+import android.graphics.BitmapFactory;
+
 public final class BitmapLess {
+
+    /**
+     * 根据reqWidth, reqHeight计算最合适的inSampleSize
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public static int $inSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // raw height and width of image
+        int rawWidth = options.outWidth;
+        int rawHeight = options.outHeight;
+
+        // calculate best sample size
+        int inSampleSize = 0;
+        if (rawHeight > reqHeight || rawWidth > reqWidth) {
+            float ratioWidth = (float) rawWidth / reqWidth;
+            float ratioHeight = (float) rawHeight / reqHeight;
+            inSampleSize = (int) Math.min(ratioHeight, ratioWidth);
+        }
+        inSampleSize = Math.max(1, inSampleSize);
+
+        return inSampleSize;
+    }
 }
