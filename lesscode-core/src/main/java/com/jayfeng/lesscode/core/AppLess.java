6afc76dc7736c092ae3aeee83c289a8d71b9e1d8
fengjian <fengjian@51muai.com>
2016-02-18 22:41:06 +0800

    add clear** method for appless

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/AppLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/AppLess.java
@@ -138,10 +138,42 @@ public final class AppLess {
      * @param context
      */
     public static void $clear(Context context) {
+        $clearCache(context);
+        $clearFiles(context);
+        $clearSharedPreference(context);
+        $clearDatabase(context);
+    }
+
+    /**
+     * 删除应用缓存目录
+     * @param context
+     */
+    public static void $clearCache(Context context) {
         FileLess.$del(context.getCacheDir(), true);
-        FileLess.$del(context.getFilesDir(), true);
-        FileLess.$del(new File("/data/data/" + context.getPackageName() + "/shared_prefs"), true);
-        FileLess.$del(new File("/data/data/" + context.getPackageName() + "/databases"), true);
         FileLess.$del(context.getExternalCacheDir(), true);
+    }
+
+    /**
+     * 删除应用文件目录
+     * @param context
+     */
+    public static void $clearFiles(Context context) {
+        FileLess.$del(context.getFilesDir(), true);
+    }
+
+    /**
+     * 删除应用Shared Prefrence目录
+     * @param context
+     */
+    public static void $clearSharedPreference(Context context) {
+        FileLess.$del(new File("/data/data/" + context.getPackageName() + "/shared_prefs"), true);
+    }
+
+    /**
+     * 删除应用数据库目录
+     * @param context
+     */
+    public static void $clearDatabase(Context context) {
+        FileLess.$del(new File("/data/data/" + context.getPackageName() + "/databases"), true);
     }
 }
