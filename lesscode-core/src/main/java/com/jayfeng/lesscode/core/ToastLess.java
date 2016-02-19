c5610f53481e3cdfa47b18e41bc057292f69648d
jayfeng <673592063@qq.com>
2016-02-19 21:26:22 +0800

    upgrade the android studio to 2.0.0 beta 5

--- lesscode-core/src/main/java/com/jayfeng/lesscode/core/ToastLess.java
+++ lesscode-core/src/main/java/com/jayfeng/lesscode/core/ToastLess.java
@@ -10,10 +10,10 @@ public final class ToastLess {
 
     public static void $(Context context, String message) {
         Toast.makeText(context.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    };
+    }
 
     public static void $(Context context, int stringId) {
         Toast.makeText(context.getApplicationContext(), stringId, Toast.LENGTH_SHORT).show();
-    };
+    }
 
 }
