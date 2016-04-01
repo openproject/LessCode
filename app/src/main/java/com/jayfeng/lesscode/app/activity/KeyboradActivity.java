508f5972dff31c6d032b358d1325702a8c419383
fengjian <fengjian@51muai.com>
2016-04-01 15:57:38 +0800

    add sample code for KeyBoradLess

--- app/src/main/java/com/jayfeng/lesscode/app/activity/KeyboradActivity.java
+++ app/src/main/java/com/jayfeng/lesscode/app/activity/KeyboradActivity.java
@@ -2,14 +2,42 @@ package com.jayfeng.lesscode.app.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.jayfeng.lesscode.app.R;
+import com.jayfeng.lesscode.core.KeyBoardLess;
+import com.jayfeng.lesscode.core.ViewLess;
 
 public class KeyboradActivity extends Activity {
+
+    private EditText editText;
+    private Button showKeyboradButton;
+    private Button hideKeyboradButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyborad);
+
+        editText = ViewLess.$(this, R.id.edit);
+        showKeyboradButton = ViewLess.$(this, R.id.show);
+        hideKeyboradButton = ViewLess.$(this, R.id.hide);
+
+        showKeyboradButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeyBoardLess.$show(KeyboradActivity.this, editText);
+            }
+        });
+
+        hideKeyboradButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KeyBoardLess.$hide(KeyboradActivity.this, editText);
+            }
+        });
+
     }
 }
